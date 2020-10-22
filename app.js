7ae0aaca85a6bb55ca9a150b3de7609a8d9f8699
Maryam-Taer <mt.taer2001@gmail.com>
2020-10-21 19:03:52 -0700

    Submission Form CSS

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Available path /submission , /submissionList" });
+});
+
+require('./routes/router')(app);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000.");
+});
---
+++ routes/router.js
@@ -0,0 +1,10 @@
+var submissionController = require("../controllers/submissionController");
+
+
+module.exports = app => {
+  app.get("/submission", submissionController.index);
+  // Create a new Submission
+  app.post("/submission/create", submissionController.create);
+  // Fetch all the data
+  app.get("/submissionList", submissionController.getAll)
+};
